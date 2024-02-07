27e4a04cd6dc5b721b80c609da2430c8b08be7c8
Garrett Keyes <garrett.keyes@gmail.com>
2024-02-06 22:23:41 -0500

    adding database interaction

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -20,6 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         FirebaseApp.configure()
+        
+        let db = Firestore.firestore()
+        
+        print(db)
         return true
     }
 
